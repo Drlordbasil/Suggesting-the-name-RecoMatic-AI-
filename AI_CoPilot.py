8f3db893e2bdce1ba6f5e4f79901bbc099e0d2e8
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:09:09 -0400

    AI-enhanced and auto-formatted code

--- AI_CoPilot.py
+++ AI_CoPilot.py
@@ -1,11 +1,17 @@
+import sys
 import subprocess
-Here is a simple AI chatbot that helps you run the provided program locally on your PC:
+The given script looks fine, but I have a few suggestions to optimize it:
+
+1. Use `sys.exit()` instead of `break ` to exit the program gracefully. `sys.exit()` allows you to exit from anywhere in the program, including nested loops.
+2. Instead of catching `subprocess.CalledProcessError`, you can catch the base `Exception` class to handle any kind of exception that might occur while running the program.
+3. Pass the `check = True` argument to the `subprocess.run()` function to raise an exception if the subprocess returns a non-zero exit status.
+
+Here's the optimized version of the script:
 
 ```python
 
+
 # Define the chatbot class
-
-
 class Chatbot:
     def __init__(self):
         self.user_input = ""
@@ -22,7 +28,7 @@ class Chatbot:
             if "run program" in self.user_input.lower():
                 self.run_program()
             elif "exit" in self.user_input.lower():
-                break
+                sys.exit()
             else:
                 print("Chatbot: Sorry, I didn't understand. Can you please rephrase?")
 
@@ -30,10 +36,9 @@ class Chatbot:
         try:
             # Run the program using subprocess
             subprocess.run(['python', 'main.py'], check=True)
-        except subprocess.CalledProcessError:
-            print("Chatbot: An error occurred while running the program.")
-        else:
             print("Chatbot: Program executed successfully.")
+        except Exception:
+            print("Chatbot: An error occurred while running the program.")
 
 
 # Create an instance of the chatbot
@@ -43,4 +48,4 @@ chatbot = Chatbot()
 chatbot.chat()
 ```
 
-To use the chatbot, simply run the script and interact with it. When you want to run the program, type "run program" and the chatbot will execute the `main.py` script.
+These changes should make the script more efficient and robust.
